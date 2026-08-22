9b731dc05fcba1d19c435fc21bf7a3d9c11df8dd
Yamila Holguin <yamila.holguin@ister.edu.ec>
2026-08-21 23:31:53 -0500

    Configuración de Swagger y pruebas de API REST

--- BikeStore/Program.cs
+++ BikeStore/Program.cs
@@ -29,13 +29,16 @@ builder.Services.AddCors(options =>
    });
 
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
